f35516d4cc4ebe6c425712ca606a868f91bd04a0
Brian Bridge <bbridge@fastresponse.org>
2013-10-21 15:39:30 -0700

    ajaxifying the events loading

--- community/events.php
+++ community/events.php
@@ -1,138 +1,3 @@
-<?php
-
-// load database code
-
-require_once($_SERVER['DOCUMENT_ROOT'] . '/php/dbconn.php');
-$handle = db_connect('events');
-
-
-// events code
-
-function format_event($handle, $event) {
-  /*
-   * $event = array(
-   *   'id' => integer,
-   *   'date' => 'date in YYYY-MM-DD format',
-   *   'longdate' => 'date in MMM DD, YYYY format',
-   *   'title' => 'string',
-   *   'body' => 'html?',
-   *   'thumbnail' => 'image url',
-   *   'programs' => array(
-   *     'EMT', 'CPT', ...
-   *   ),
-   *   'imagesize' => 'small/medium/large',
-   *   'images' => array(
-   *     'image url 1', 'image url 2', ...
-   *   ),
-   *   'links' => array(
-   *     'link text 1' => 'link url 1',
-   *     'link text 2' => 'link url 2',
-   *   )
-   * );
-   */
-
-  // all text from db_query is sent through htmlentities()
-  // html_entity_decode() counteracts that so we can display html
-  $event['body'] =
-    "<p>" .
-    str_replace(
-      "\n",
-      "</p>\n<p>",
-      html_entity_decode($event['body'])
-    ) .
-    "</p>"
-  ;
-
-  $event['programs'] = str_replace(',', ', ', $event['programs']);
-
-  if (!isset($event['thumbnail']))
-    $event['thumbnail'] = '';
-  
-  if ($event['thumbnail'] != '')
-    $event['thumbnail'] =
-      "<img src='{$event['thumbnail']}' alt='' class='event-thumbnail' />";
-
-  if (!isset($event['imagesize']))
-    $event['imagesize'] = '';
-
-  if (isset($event['images'])) {
-    $event['images'] = explode(',', $event['images']);
-    $tmp = '';
-    foreach ($event['images'] as $image) {
-      $tmp .= "<img src='$image' alt='' " .
-	      "class='event-image size-{$event['imagesize']}' />";
-    }
-    $event['images'] = $tmp;
-  }
-  else
-    $event['images'] = '';
-
-  if (isset($event['links'])) {
-    $event['links'] = explode(',', $event['links']);
-    $tmp = '';
-    foreach ($event['links'] as $linktext) {
-      $data = query_button($handle, $linktext);
-      if (!isset($data) || !count($data)) continue;
-      $linkurl = $data['link'];
-      $tmp .= "<a href='$linkurl' class='event-link'>$linktext</a>";
-    }
-    $event['links'] = $tmp;
-  }
-  else
-    $event['links'] = '';
-
-  foreach ($event as $key => $val) {
-    if (is_int($key) || $key == 'html' || $key == 'thumbnail') continue;
-    $classes = "event-$key";
-    if ($val == '') $classes .= ' empty';
-    $event[$key] = "<div class='$classes'>$val</div>";
-  }
-
-  return $event;
-}
-
-function get_events($handle, $max) {
-  $defs = array(
-    'connerr' => array(
-      'id' => 0,
-      'title' => 'Events',
-      'body' => 'There was a problem connecting to the server. ' .
-                'Please try again later.',
-    ),
-    'notfound' => array(
-      'id' => 0,
-      'title' => 'Events',
-      'body' => 'There were no events found.',
-    ),
-  );
-
-  if (!isset($handle)) {
-    $results = array($defs['connerr']);
-  }
-  else if (!isset($max)) {
-    $results = array($defs['notfound']);
-  }
-  else {
-    $results = query_recent_events($handle, $max);
-  }
-
-  if (!isset($results))
-    $results = array($defs['notfound']);
-
-  // loop variable by reference
-  // lets us change the value in the original array
-  foreach ($results as &$result) {
-    $result = format_event($handle, $result);
-  }
-  unset($result); // break reference
-
-  return $results;
-}
-
-$events = get_events($handle, 8);
-
-?>
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
@@ -334,45 +199,28 @@ $events = get_events($handle, 8);
 
       <div class="rightsidebar2">
         <div id="event-calendar"></div>
-	<script type="text/javascript">
-	  $(document).ready(function() {
-	    $("#event-calendar").eventCalendar({
-	      eventsjson: '/js/eventCalendar/json/event.humanDate.json.php',
-	      jsonDateFormat: 'human',
-	      eventsLimit: 0,
-	      startWeekOnMonday: false,
-	      alwaysHideDescription: true,
-	      txt_NextEvents: "Upcoming Classes and Events"
-	    });
-	  });
-	</script>
+	      <script type="text/javascript">
+	        $(document).ready(function() {
+	          $("#event-calendar").eventCalendar({
+	            eventsjson: '/js/eventCalendar/json/event.humanDate.json.php',
+	            jsonDateFormat: 'human',
+	            eventsLimit: 0,
+	            startWeekOnMonday: false,
+	            alwaysHideDescription: true,
+	            txt_NextEvents: "Upcoming Classes and Events"
+	          });
+	        });
+	      </script>
       </div>
 
       <div class="leftcontent2">
 
         <h1 style="text-align: center;">Community Events</h1>
 
-        <?php foreach ($events as $event): ?>
-          <pre><?php //print_r($event); ?></pre>
-          <div class="event article-box">
-            <div class="title">
-              <div class="title-border">
-                <table cellpadding="0"><tbody><tr>
-                  <td class="left"><h2><?= $event['longdate'] ?></h2></td>
-                  <td class="center"><h1><?= $event['title'] ?></h1></td>
-                  <td class="right"><h2><?= $event['programs'] ?></h2></td>
-                </tr></tbody></table>
-              </div>
-            </div>
-            <div class="body">
-              <?= $event['thumbnail'] ?>
-              <?= $event['body'] ?>
-              <?= $event['links'] ?>
-              <?= $event['images'] ?>
-            </div>
-          </div>
-        <?php endforeach; ?>
-
+        <script type="text/javascript">
+          // $.ajax call to /php/events_ajax.php
+          // what to show by default?? <?php include(/php/events_ajax.php) ?> ???
+        </script>
 	    </div> <!-- /leftcontent -->
 
       <div class="clearfix"></div>
---
+++ php/events_ajax.php
@@ -0,0 +1,152 @@
+<?php
+
+require_once($_SERVER['DOCUMENT_ROOT'] . '/php/dbconn.php');
+
+$handle = db_connect('events');
+
+function format_event($handle, $event) {
+  /*
+   * $event = array(
+   *   'id' => integer,
+   *   'date' => 'date in YYYY-MM-DD format',
+   *   'longdate' => 'date in MMM DD, YYYY format',
+   *   'title' => 'string',
+   *   'body' => 'html?',
+   *   'thumbnail' => 'image url',
+   *   'programs' => array(
+   *     'EMT', 'CPT', ...
+   *   ),
+   *   'imagesize' => 'small/medium/large',
+   *   'images' => array(
+   *     'image url 1', 'image url 2', ...
+   *   ),
+   *   'links' => array(
+   *     'link text 1' => 'link url 1',
+   *     'link text 2' => 'link url 2',
+   *   )
+   * );
+   */
+
+  // all text from db_query is sent through htmlentities()
+  // html_entity_decode() counteracts that so we can display html
+  $event['body'] =
+    "<p>" .
+    str_replace(
+      "\n",
+      "</p>\n<p>",
+      html_entity_decode($event['body'])
+    ) .
+    "</p>"
+  ;
+
+  $event['programs'] = str_replace(',', ', ', $event['programs']);
+
+  if (!isset($event['thumbnail']))
+    $event['thumbnail'] = '';
+  
+  if ($event['thumbnail'] != '')
+    $event['thumbnail'] =
+      "<img src='{$event['thumbnail']}' alt='' class='event-thumbnail' />";
+
+  if (!isset($event['imagesize']))
+    $event['imagesize'] = '';
+
+  if (isset($event['images'])) {
+    $event['images'] = explode(',', $event['images']);
+    $tmp = '';
+    foreach ($event['images'] as $image) {
+      $tmp .= "<img src='$image' alt='' " .
+	      "class='event-image size-{$event['imagesize']}' />";
+    }
+    $event['images'] = $tmp;
+  }
+  else
+    $event['images'] = '';
+
+  if (isset($event['links'])) {
+    $event['links'] = explode(',', $event['links']);
+    $tmp = '';
+    foreach ($event['links'] as $linktext) {
+      $data = query_button($handle, $linktext);
+      if (!isset($data) || !count($data)) continue;
+      $linkurl = $data['link'];
+      $tmp .= "<a href='$linkurl' class='event-link'>$linktext</a>";
+    }
+    $event['links'] = $tmp;
+  }
+  else
+    $event['links'] = '';
+
+  foreach ($event as $key => $val) {
+    if (is_int($key) || $key == 'html' || $key == 'thumbnail') continue;
+    $classes = "event-$key";
+    if ($val == '') $classes .= ' empty';
+    $event[$key] = "<div class='$classes'>$val</div>";
+  }
+
+  return $event;
+}
+
+function get_events($handle, $max) {
+  $defs = array(
+    'connerr' => array(
+      'id' => 0,
+      'title' => 'Events',
+      'body' => 'There was a problem connecting to the server. ' .
+                'Please try again later.',
+    ),
+    'notfound' => array(
+      'id' => 0,
+      'title' => 'Events',
+      'body' => 'There were no events found.',
+    ),
+  );
+
+  if (!isset($handle)) {
+    $results = array($defs['connerr']);
+  }
+  else if (!isset($max)) {
+    $results = array($defs['notfound']);
+  }
+  else {
+    $results = query_recent_events($handle, $max);
+  }
+
+  if (!isset($results))
+    $results = array($defs['notfound']);
+
+  // loop variable by reference
+  // lets us change the value in the original array
+  foreach ($results as &$result) {
+    $result = format_event($handle, $result);
+  }
+  unset($result); // break reference
+
+  return $results;
+}
+
+$events = get_events($handle, 8);
+
+foreach ($events as $event) {
+  $out = <<<OUT
+<div class='event article-box'>
+  <div class='title'>
+    <div class='title-border'>
+      <table cellpadding='0'><tbody><tr>
+        <td class='left'><h2>{$event['longdate']}</h2></td>
+        <td class='center'><h1>{$event['title']}</h1></td>
+        <td class='right'><h2>{$event['programs']}</h2></td>
+      </tr></tbody></table>
+    </div>
+  </div>
+  <div class='body'>
+    {$event['thumbnail']}
+    {$event['body']}
+    {$event['links']}
+    {$event['images']}
+  </div>
+</div>
+OUT;
+}
+
+?>
