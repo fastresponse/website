5dea014c9d4a987f4cbe5ba1a64d597d5c23dcb7
Brian Bridge <bbridge@fastresponse.org>
2014-12-17 17:42:22 -0800

    added temp new index for spt

---
+++ classes/career_courses/sterile_processing_tech/index_new.php
@@ -0,0 +1,22 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'] . '/include/course_vars.php');
+
+$page_title = 'Sterile Processing Technician';
+
+$course_name = 'Sterile Processing';
+$course_abbr = 'SPT';
+$course_title = 'Sterile Processing Technician';
+$course_types = array('Part-time');
+
+$links = array(
+  'Immunizations',
+  'IAHCSMM CRCST Information',
+);
+
+$zip_radius = 50;
+
+module_remove( array('Externship', 'Testimonials') );
+module_move('Admissions Procedures', 'left', 4);
+
+require_once($_SERVER['DOCUMENT_ROOT'] . '/include/course_template.php');
+?>
